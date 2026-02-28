4aa556e1554000de12cc3f4c4bde6ca868ce7b46
flipsbone <219644104+Flipsbone@users.noreply.github.com>
2026-02-28 01:05:00 +0100

    add last version maze_generate.py at dev branch

--- mazegen/maze_generate.py
+++ mazegen/maze_generate.py
@@ -22,12 +22,16 @@ class MazeGenerator:
         self.perfect: bool = perfect
         self.seed: Optional[str] = seed
         self.algorithm: Optional[str] = algorithm
+
         self.pattern_centered_coords: list[tuple[int, int]] = []
         self.pattern_width: int = 0
         self.pattern_height: int = 0
+
         self.create_pattern()
         self.check_if_entry_or_exit_in_pattern()
+
         self._rng: random.Random = random.Random(seed)
+
         self.maze_grid: list[list[int]] = self.initialize_maze()
         self.visited_grid = self.generate_visited_grid()
         self.generate_maze()
@@ -37,20 +41,17 @@ class MazeGenerator:
         visited_grid: list[list[bool]] = []
         visited_grid = [[False] * self.width for _ in range(self.height)]
         if self.can_fit_pattern():
-            for pattern_y, pattern_x in self.pattern_centered_coords:
-                visited_grid[pattern_y][pattern_x] = True
+            for pattern_row, pattern_col in self.pattern_centered_coords:
+                visited_grid[pattern_row][pattern_col] = True
+
         # create maze sides
         else:
             for col in range(self.width):
-                if col == 0:
-                    visited_grid[0][col] = True
-                elif col == self.width - 1:
-                    visited_grid[col][-1] = True
+                visited_grid[0][col] = True
+                visited_grid[self.height - 1][col] = True
             for row in range(self.height):
-                if row == 0:
-                    visited_grid[row][0] = True
-                if row == self.height - 1:
-                    visited_grid[row][-1] = True
+                visited_grid[row][0] = True
+                visited_grid[row][self.width - 1] = True
 
         return visited_grid
 
@@ -58,60 +59,62 @@ class MazeGenerator:
         return [[15] * self.width for _ in range(self.height)]
 
     def generate_maze(self) -> None:
-        stack: list[tuple] = []
+        stack: list[tuple[int, int]] = []
+        entry_row, entry_col = self.entry
 
-        entry_y, entry_x = self.entry
-        self.visited_grid[entry_y][entry_x] = True
-        stack.append((entry_y, entry_x))
+        self.visited_grid[entry_row][entry_col] = True
+        stack.append((entry_row, entry_col))
 
+        # DFS Backtracking loop: explore random unvisited neighbors,
+        # or backtrack when hitting a dead end
         while stack:
-            current_y, current_x = stack[-1]
+            current_row, current_col = stack[-1]
             valid_neighbors: list = []
 
             # North : y - 1
             if (
-                current_y - 1 >= 0
-                and self.visited_grid[current_y - 1][current_x] is False
+                current_row - 1 >= 0
+                and self.visited_grid[current_row - 1][current_col] is False
             ):
-                valid_neighbors.append(("North", current_y - 1, current_x))
+                valid_neighbors.append(("North", current_row - 1, current_col))
             # South : y + 1
             if (
-                current_y + 1 < self.height
-                and self.visited_grid[current_y + 1][current_x] is False
+                current_row + 1 < self.height
+                and self.visited_grid[current_row + 1][current_col] is False
             ):
-                valid_neighbors.append(("South", current_y + 1, current_x))
+                valid_neighbors.append(("South", current_row + 1, current_col))
             # East : x + 1
             if (
-                current_x + 1 < self.width
-                and self.visited_grid[current_y][current_x + 1] is False
+                current_col + 1 < self.width
+                and self.visited_grid[current_row][current_col + 1] is False
             ):
-                valid_neighbors.append(("East", current_y, current_x + 1))
+                valid_neighbors.append(("East", current_row, current_col + 1))
             # West : x - 1
             if (
-                current_x - 1 >= 0
-                and self.visited_grid[current_y][current_x - 1] is False
+                current_col - 1 >= 0
+                and self.visited_grid[current_row][current_col - 1] is False
             ):
-                valid_neighbors.append(("West", current_y, current_x - 1))
+                valid_neighbors.append(("West", current_row, current_col - 1))
 
             if valid_neighbors:
-                chosen_direction, next_y, next_x = random.choice(
+                chosen_direction, next_row, next_col = random.choice(
                     valid_neighbors
                 )
 
-                self.visited_grid[next_y][next_x] = True
-                stack.append((next_y, next_x))
+                self.visited_grid[next_row][next_col] = True
+                stack.append((next_row, next_col))
                 if chosen_direction == "North":
-                    self.maze_grid[current_y][current_x] -= 1
-                    self.maze_grid[next_y][next_x] -= 4
+                    self.maze_grid[current_row][current_col] -= 1
+                    self.maze_grid[next_row][next_col] -= 4
                 elif chosen_direction == "South":
-                    self.maze_grid[current_y][current_x] -= 4
-                    self.maze_grid[next_y][next_x] -= 1
+                    self.maze_grid[current_row][current_col] -= 4
+                    self.maze_grid[next_row][next_col] -= 1
                 elif chosen_direction == "East":
-                    self.maze_grid[current_y][current_x] -= 2
-                    self.maze_grid[next_y][next_x] -= 8
-                else:
-                    self.maze_grid[current_y][current_x] -= 8
-                    self.maze_grid[next_y][next_x] -= 2
+                    self.maze_grid[current_row][current_col] -= 2
+                    self.maze_grid[next_row][next_col] -= 8
+                elif chosen_direction == "West":
+                    self.maze_grid[current_row][current_col] -= 8
+                    self.maze_grid[next_row][next_col] -= 2
             else:
                 stack.pop()
 
@@ -126,7 +129,7 @@ class MazeGenerator:
             "..#.###",
         ]
 
-        # fill pattern_coords with the position of
+        # fill pattern_coords with the position of "#"
         for row_idx, row_string in enumerate(pattern_design):
             for col_idx, char in enumerate(row_string):
                 if char == "#":
@@ -145,6 +148,7 @@ class MazeGenerator:
         else:
             start_pattern_col: int = (self.width - self.pattern_width) // 2
             start_pattern_row: int = (self.height - self.pattern_height) // 2
+
             for row, col in pattern_coords:
                 absolute_row: int = start_pattern_row + row
                 absolute_col: int = start_pattern_col + col
@@ -164,18 +168,14 @@ class MazeGenerator:
                 )
 
     def can_fit_pattern(self) -> bool:
-        if (
-            self.width >= self.pattern_width + 2
-            and self.height >= self.pattern_height + 2
-        ):
-            return True
-        return False
+        return (self.width >= self.pattern_width + 2 and
+                self.height >= self.pattern_height + 2)
 
     def solve(self) -> str | None:
         path_solve: list[tuple[int, int, str]] = []
         entry_row, entry_col = self.entry
-        path_solve.append((entry_row, entry_col, ""))
 
+        path_solve.append((entry_row, entry_col, ""))
         self.visited_solve[entry_row][entry_col] = True
 
         # BFS loop: continue as long as there are cells left to explore
@@ -212,3 +212,9 @@ class MazeGenerator:
                     current_row, current_col - 1, current_path + "W"))
 
         return None
+
+    def regenerate(self) -> None:
+        self.maze_grid = self.initialize_maze()
+        self.visited_grid = self.generate_visited_grid()
+        self.generate_maze()
+        self.visited_solve = self.generate_visited_grid()
